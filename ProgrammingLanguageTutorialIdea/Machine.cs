e746dc3c1057e15ac4a2d4d5ff8cf1c7a41f8f56
cashsignsesh <68089912+cashsignsesh@users.noreply.github.com>
2021-05-28 00:37:24 -0400

    Initial commit

---
+++ ProgrammingLanguageTutorialIdea/Machine.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Elite
+ * Date: 4/3/2021
+ * Time: 6:04 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ProgrammingLanguageTutorialIdea {
+	
+	public enum Machine : ushort /* ushort == UInt16 */ {
+		
+		Unknown,
+		Alpha=388,
+		Alpha64=644,
+		AM33=467,
+		Amd64=34404,
+		Arm=448,
+		Arm64=43620,
+		ArmThumb2=452,
+		Ebc=3772,
+		I386=332,
+		IA64=512,
+		M32R=36929,
+		MIPS1=614,
+		MipsFpu=870,
+		MipsFpu16=1126,
+		PowerPC=496,
+		PowerPCFP=497,
+		SH3=418,
+		SH3Dsp=419,
+		SH3E=420,
+		SH4=422,
+		SH5=424,
+		Thumb=450,
+		Tricore=1312,
+		WceMipsV2=361
+		
+	}
+	
+}
---
+++ ProgrammingLanguageTutorialIdea/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Elite
+ * Date: 5/13/2021
+ * Time: 12:04 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace ProgrammingLanguageTutorialIdea {
+	
+	internal class Program {
+		
+		[DllImport("ImageHlp.dll")]
+		static extern private UInt32 MapFileAndCheckSum (String Filename,out UInt32 HeaderSum,out UInt32 CheckSum);
+		
+		public static void Main (String[] args) {
+			
+			Console.WriteLine(Marshal.SizeOf(typeof(PEHeader)));
+			
+			UInt32 memAddress=0x004001000;
+			
+			List<Byte> opcodes=new List<Byte>(),importOpcodes=null,finalBytes=new List<Byte>();
+			opcodes.Add(0xC3);
+			++memAddress;
+			
+			Console.WriteLine(memAddress.ToString("X"));
+			PEHeader hdr=PEHeaderFactory.newHdr(opcodes,importOpcodes,memAddress,0);
+			
+			while(opcodes.Count%512!=0)
+				opcodes.Add(0);
+			
+			finalBytes.AddRange(hdr.toBytes());
+			finalBytes.AddRange(opcodes);
+			if (importOpcodes!=null)
+				finalBytes.AddRange(importOpcodes);
+			
+			const String outputFilename="thing.exe";
+			
+			File.WriteAllBytes(outputFilename,finalBytes.ToArray());
+			
+			UInt32 checkSum;
+			Program.MapFileAndCheckSum(outputFilename,out checkSum,out checkSum);
+			using (FileStream fs=File.Open(outputFilename,FileMode.Open)) {
+				
+				fs.Seek(216,SeekOrigin.Current);
+				fs.Write(BitConverter.GetBytes(checkSum),0,4);
+				
+			}
+			
+			halt:goto halt;
+			
+		}
+		
+	}
+	
+}
